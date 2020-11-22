1f09a0d906a122d9a6a68da623a9298060c54a2d
Apolo Freitas <apolobrener11@gmail.com>
2020-11-22 14:58:09 -0400

    feat: add api integration to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,43 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, Accordion } from 'react-bootstrap'
 import { FaRegFlag } from 'react-icons/fa'
 import { BiChip } from 'react-icons/bi'
 import { HiOutlineLocationMarker } from 'react-icons/hi'
 import { AiOutlineToTop } from 'react-icons/ai'
 
-import styles from './styles.module.scss'
+import api from 'src/services/api'
+
 import TopBar from 'src/components/TopBar'
 
+import styles from './styles.module.scss'
+
+interface Pothole {
+  _id: string
+  coordinates: {
+    latitude: number
+    longitude: number
+  }
+  senderIMEI: number
+  sensorDistance: number
+  info: string
+}
+
 export default function Home() {
-  const arduinos = [
-    {
-      coordinates: {
-        latitude: -22.9140693,
-        longitude: -43.5860659,
-      },
-      senderIMEI: 12312,
-      sensorDistance: 4.32,
-      info: 'informações adicionais',
-    },
-    {
-      coordinates: {
-        latitude: 1231,
-        longitude: 1231,
-      },
-      senderIMEI: 12312,
-      sensorDistance: 4.32,
-      info: 'informações adicionais',
-    },
-    {
-      coordinates: {
-        latitude: 1231,
-        longitude: 1231,
-      },
-      senderIMEI: 12312,
-      sensorDistance: 4.32,
-      info: 'informações adicionais',
-    },
-  ]
+  const [potholes, setPotholes] = useState<Pothole[]>([])
+
+  useEffect(() => {
+    api
+      .get('pothole/getAll')
+      .then(({ data: { potholes } }) => setPotholes(potholes))
+  }, [])
 
   return (
     <>
@@ -45,16 +37,16 @@ export default function Home() {
       <main className={styles.container}>
         <Accordion className={styles.accordion}>
           <h1 className={styles.accordionTitle}>Registros:</h1>
-          {arduinos.map((arduino, index) => {
+          {potholes.map((pothole, index) => {
             return (
-              <Card key={index}>
+              <Card key={pothole._id}>
                 <Accordion.Toggle
                   as={Card.Header}
                   eventKey={String(index)}
                   className={styles.cardHeader}
                 >
                   <FaRegFlag size={25} />
-                  {arduino.info}
+                  {pothole.info}
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={String(index)}>
                   <Card.Body className={styles.cardBody}>
@@ -64,18 +56,18 @@ export default function Home() {
                         <a
                           target="_blank"
                           rel="noopener noreferrer"
-                          href={`http://maps.google.com/maps?q=${arduino.coordinates.latitude},${arduino.coordinates.longitude}`}
+                          href={`http://maps.google.com/maps?q=${pothole.coordinates.latitude},${pothole.coordinates.longitude}`}
                         >
-                          {`${arduino.coordinates.latitude}, ${arduino.coordinates.longitude}`}
+                          {`${pothole.coordinates.latitude}, ${pothole.coordinates.longitude}`}
                         </a>
                       </div>
                       <div>
                         <BiChip size={22} fontSizeAdjust="none" />
-                        {arduino.senderIMEI}
+                        {pothole.senderIMEI}
                       </div>
                       <div>
                         <AiOutlineToTop size={22} fontSizeAdjust="none" />
-                        {arduino.sensorDistance}cm
+                        {pothole.sensorDistance}cm
                       </div>
                     </div>
                   </Card.Body>
